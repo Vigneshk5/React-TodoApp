7c032d034d585f7ef6c7aeb2fc5914509931eefd
Vigneshk5 <vigneshpatali5@gmail.com>
2023-02-16 20:36:17 +0530

    added styles

--- src/App.js
+++ src/App.js
@@ -24,23 +24,60 @@ function App() {
   }
 
   return (
-    <div className="container mx-auto bg-gray-200 rounded-xl">
-      <h1>todo list</h1>
-      <input
-        onKeyDown={tryToCheckForEnterKey}
-        type="text"
-        value={task}
-        onChange={(event) => {
-          setTask(event.target.value);
-        }}
-      />
-      <button onClick={createToDo}>Create</button>
+    <div>
+      <div
+        className="
+        h-100
+        w-full
+        flex
+        items-center
+        justify-center
+        bg-teal-lightest
+        font-sans"
+      >
+        <h1 className="flex-no-shrink p-2 border-2 rounded  text-indigo-500 background-transparent font-bold uppercase ">
+          todolist
+        </h1>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
+          onKeyDown={tryToCheckForEnterKey}
+          type="text"
+          value={task}
+          onChange={(event) => {
+            setTask(event.target.value);
+          }}
+        />
+        <button
+          className="flex-no-shrink p-2 border-2 rounded text-teal-500 background-transparent font-bold uppercase "
+          onClick={createToDo}
+        >
+          Create
+        </button>
+      </div>
       <ul>
         {todos.map((item, index) => {
           return (
-            <div key={item.id}>
-              <li>{item.todo}</li>
-              <button onClick={() => deleteItem(item.id)}>delete</button>
+            <div
+              className="
+        h-100
+        w-full
+        flex
+        items-center
+        justify-center
+        bg-teal-lightest
+        font-sans"
+            >
+              <div key={item.id}>
+                <ls className="shadow appearance-none border rounded  py-2 px-3 mr-4 text-grey-darker">
+                  {item.todo}
+                </ls>
+                <button
+                  className="flex-no-shrink p-2 border-2 rounded text-red-500 background-transparent font-bold uppercase "
+                  onClick={() => deleteItem(item.id)}
+                >
+                  delete
+                </button>
+              </div>
             </div>
           );
         })}
